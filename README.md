bd93c2b323642d887c802b02edc3b72397d05e4e
Khesa <afifah.luthfianisa2042@students.unila.ac.id>
2022-12-24 00:27:04 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-For using 'build' command you need to install: 'webpack' package.
+# Catatan Untuk Menjalankan Project
+
+## Pastikan php yg terinstal minimal versi `8.0`
+
+#
+### Buat Database di localhost / phpmyadmin 
+```bash
+CREATE DATABASE `lab_kimia` /*!40100 DEFAULT CHARACTER SET utf8mb4 */
+```
+#
+### Jalankan syntax ini di terminal editor code (ex. Vscode) untuk menggenerate table 
+
+```bash
+php artisan migrate:fresh
+```
+
+#
+### Jalankan Seeder (Dummy Data)
+```bash
+php artisan db:seed
+```
+
+#
+### Test dan jalankan Aplikasi 
+```bash
+php artisan serve
+```
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,24 +25,6 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $first_name = $this->faker->firstName;
-        $last_name = $this->faker->lastName;
-        $name = $first_name.' '.$last_name;
-
-        return [
-            'first_name'        => $first_name,
-            'last_name'         => $last_name,
-            'name'              => $name,
-            'email'             => $this->faker->unique()->safeEmail(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token'    => Str::random(10),
-            'mobile'            => $this->faker->phoneNumber,
-            'date_of_birth'     => $this->faker->date,
-            'avatar'            => 'img/default-avatar.jpg',
-            'gender'            => $this->faker->randomElement(['Male', 'Female', 'Other']),
-            'email_verified_at' => now(),
-            'created_at'        => now(),
-            'updated_at'        => now(),
-        ];
+        
     }
 }
